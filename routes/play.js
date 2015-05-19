ece257788fa1028eaa2144e3f212565992c74cdb
nibuen <nibuenss@yahoo.com>
2015-05-19 15:34:38 -0700

    Updating paths to prepare for game module

--- routes/play.js
+++ routes/play.js
@@ -1,20 +1,18 @@
 var express = require('express');
+var wordGame = require('chittydbeefy-word-game');
 var router = express.Router();
 
 router.post('/', function (req, res) {
     console.log("Post Data:"+ req.body.playerName);
-    var playerArr = playerData.players;
-    playerArr.push(req.body.playerName);
-    playerData.players = playerArr;
+    wordGame.addPlayer(req.body.playerName);
 
-
-    fs.writeFile('./users.json', JSON.stringify(playerData), function (err,data) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(playerData);
-        res.send(JSON.stringify(playerData));
-    });
+    //fs.writeFile('./users.json', JSON.stringify(playerData), function (err,data) {
+    //    if (err) {
+    //        return console.log(err);
+    //    }
+    //    console.log(playerData);
+    //    res.send(JSON.stringify(playerData));
+    //});
     // res.sendFile(path.join(__dirname + '/../views/game_screen.html'));
 });
 
