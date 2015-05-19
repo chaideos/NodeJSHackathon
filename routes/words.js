ceb3227534412b45b9548f4045706e46fdf546da
nibuen <nibuenss@yahoo.com>
2015-05-19 12:13:28 -0700

    Adding word dictionary dependency.

--- routes/words.js
+++ routes/words.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var wordDictionary = require('word-dictionary');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET Submitted word. */
 router.get('/', function(req, res, next) {
   if(wordDictionary.isWord("Hello")) {
+    res.status(200);
+  }
+  else {
+    res.status(404);
+  }
+  res.send();
+});
+
+/* POST Submitted word. */
+router.post('/', function(req, res, next) {
+
+  // TODO bodyparse.json() get data
+
+  if(wordDictionary.isWord('abnegate')) {
     res.status(200);
   }
   else {
