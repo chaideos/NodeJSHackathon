595d7c2b283adebaccb80b59dd39b8ad614a76b9
nibuen <nibuenss@yahoo.com>
2015-05-19 13:53:31 -0700

    Dictionary is now working.

--- routes/words.js
+++ routes/words.js
@@ -1,31 +1,19 @@
 var express = require('express');
 var wordDictionary = require('word-dictionary');
 var bodyParser = require('body-parser');
+var url = require('url');
 var router = express.Router();
 
 /* GET Submitted word. */
-router.get('/', function(req, res, next) {
-  if(wordDictionary.isWord("Hello")) {
-    res.status(200);
+router.get('/:word', function(req, res, next) {
+  console.log(req.params.word);
+
+  if(wordDictionary.isWord(req.params.word)) {
+    res.sendStatus(200);
   }
   else {
-    res.status(404);
+    res.sendStatus(404);
   }
-  res.send();
-});
-
-/* POST Submitted word. */
-router.post('/', function(req, res, next) {
-
-  // TODO bodyparse.json() get data
-
-  if(wordDictionary.isWord('abnegate')) {
-    res.status(200);
-  }
-  else {
-    res.status(404);
-  }
-  res.send();
 });
 
 module.exports = router;
